7126eb1b712e32c7a42b16d71a58c358dbb85693
Seb-Olsen <65488474+Seb-Olsen@users.noreply.github.com>
2025-02-26 15:59:12 +0100

    Renamed to RealtimeOpenAi

--- Package.swift
+++ Package.swift
@@ -3,22 +3,23 @@
 import PackageDescription
 
 let package = Package(
-	name: "OpenAI",
-	platforms: [
-		.iOS(.v17),
-		.tvOS(.v17),
-		.macOS(.v14),
-		.watchOS(.v10),
-		.visionOS(.v1),
-		.macCatalyst(.v17),
-	],
-	products: [
-		.library(name: "OpenAI", type: .static, targets: ["OpenAI"]),
-	],
-	dependencies: [
-		.package(url: "https://github.com/stasel/WebRTC.git", branch: "latest"),
-	],
-	targets: [
-		.target(name: "OpenAI", dependencies: ["WebRTC"], path: "./src"),
-	]
+    name: "OpenAI",
+    platforms: [
+        .iOS(.v17),
+        .tvOS(.v17),
+        .macOS(.v14),
+        .watchOS(.v10),
+        .visionOS(.v1),
+        .macCatalyst(.v17),
+    ],
+    products: [
+        // Rename the product to avoid conflicts
+        .library(name: "RealtimeOpenAI", type: .static, targets: ["OpenAI"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/stasel/WebRTC.git", branch: "latest"),
+    ],
+    targets: [
+        .target(name: "OpenAI", dependencies: ["WebRTC"], path: "./src"),
+    ]
 )
